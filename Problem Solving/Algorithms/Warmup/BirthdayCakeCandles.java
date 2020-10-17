ad8bcdebef32607c77fb60736ed0166fdc3bd2e2
Abhishek Sahu <61976357+abhisahugm007@users.noreply.github.com>
2020-10-17 20:11:26 +0530

    Create BirthdayCakeCandles.java

---
+++ Problem Solving/Algorithms/Warmup/BirthdayCakeCandles.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the birthdayCakeCandles function below.
+    static int birthdayCakeCandles(int[] a) {
+        int max=a[0];
+        for(int i=1; i < a.length;i++ )
+        {
+            if(max < a[i])
+                max=a[i];
+        }
+        int count=0;
+         for(int i=0; i < a.length;i++ )
+         {
+             if(a[i]==max)
+                count++;
+         }
+         return count;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arCount = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[arCount];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < arCount; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = birthdayCakeCandles(ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
+//https://www.hackerrank.com/challenges/birthday-cake-candles/problem
