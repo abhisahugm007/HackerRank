a37ca09f413eff3f649fc2b93a08ceca2079ca8c
Abhishek Sahu <61976357+abhisahugm007@users.noreply.github.com>
2020-10-17 20:35:48 +0530

    Create Picking Numbers.java

---
+++ Problem Solving/Algorithms/Implementation/Picking Numbers.java
@@ -0,0 +1,65 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'pickingNumbers' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY a as parameter.
+     */
+
+    public static int pickingNumbers(List<Integer> a) {
+        int max=0;
+        for(int i=0;i<a.size();i++){
+            int count=0;
+            for(int j=0;j<a.size();j++){
+
+                if(a.get(i)-a.get(j)==-1 || a.get(i)-a.get(j)==0 )
+                {
+                    
+                        count++;
+                }
+            }
+            if(count>=max)
+                max=count;
+                else{
+                    max=max;
+                }
+        }
+        return max;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> a = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        int result = Result.pickingNumbers(a);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+//https://www.hackerrank.com/challenges/picking-numbers/problem
