05c82b9105da1218dd1e36f1856ece19e07bf28d
Abhishek Sahu <61976357+abhisahugm007@users.noreply.github.com>
2020-10-17 20:00:28 +0530

    Create SolveMeFirst.cpp

---
+++ Problem Solving/Algorithms/Warmup/SolveMeFirst.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int solveMeFirst(int a, int b) {
+ // Hint: Type return a+b; below:
+  int sum=a+b;
+  return(sum);
+}
+
+int main() {
+  int num1, num2;
+  int sum;
+  cin>>num1>>num2;
+  sum = solveMeFirst(num1,num2);
+  cout<<sum;
+  return 0;
+}
+
+
+//https://www.hackerrank.com/challenges/solve-me-first/problem
