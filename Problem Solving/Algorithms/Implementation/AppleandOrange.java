520764e5c026180ffc44b155b5e3a99e8cd0a414
Abhishek Sahu <61976357+abhisahugm007@users.noreply.github.com>
2020-10-17 20:14:22 +0530

    Create AppleandOrange.java

---
+++ Problem Solving/Algorithms/Implementation/AppleandOrange.java
@@ -0,0 +1,77 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the countApplesAndOranges function below.
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int counta =0,counto=0;
+        for(int i=0;i<apples.length;i++)
+        {
+            apples[i]=apples[i]+a;
+            if(apples[i]>=s & apples[i]<=t)
+                counta++;
+        }
+        for(int i=0;i<oranges.length;i++)
+        {
+            oranges[i]=oranges[i]+b;
+            if(oranges[i]>=s & oranges[i]<=t)
+                counto++;
+        }
+        System.out.println(counta+"\n"+counto);
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] st = scanner.nextLine().split(" ");
+
+        int s = Integer.parseInt(st[0]);
+
+        int t = Integer.parseInt(st[1]);
+
+        String[] ab = scanner.nextLine().split(" ");
+
+        int a = Integer.parseInt(ab[0]);
+
+        int b = Integer.parseInt(ab[1]);
+
+        String[] mn = scanner.nextLine().split(" ");
+
+        int m = Integer.parseInt(mn[0]);
+
+        int n = Integer.parseInt(mn[1]);
+
+        int[] apples = new int[m];
+
+        String[] applesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < m; i++) {
+            int applesItem = Integer.parseInt(applesItems[i]);
+            apples[i] = applesItem;
+        }
+
+        int[] oranges = new int[n];
+
+        String[] orangesItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int orangesItem = Integer.parseInt(orangesItems[i]);
+            oranges[i] = orangesItem;
+        }
+
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+
+        scanner.close();
+    }
+}
+
+//https://www.hackerrank.com/challenges/apple-and-orange/problem
